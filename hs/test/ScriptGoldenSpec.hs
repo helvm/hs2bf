8ab85a64af3a6ee66f66e58057e991b2349c849f
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-02-20 14:51:13 +0100

    Update code

--- hs/test/ScriptGoldenSpec.hs
+++ hs/test/ScriptGoldenSpec.hs
@@ -26,12 +26,13 @@ test_golden = unsafePerformIO $ do
     createTest inFile = do
         let baseName = takeBaseName inFile
         let opt = Option { addrSpace = 2, verbose = False, debug = False, tolang = LangBF }
+
         output <- partialChain opt inFile $
             (error "Core not needed", error "Core not needed",
              error "GMachine not needed", error "GMachine not needed",
              error "SAM not needed", error "SAM not needed",
-             \bf -> do
-                 pure $ BSL.fromStrict $ Brainfuck.pprint bf
+             \bf -> pure $ BSL.fromString $ Brainfuck.pprint bf
             )
+
         let goldenFile = ".golden" </> "hs2bf" </> baseName <.> "bf"
         pure $ goldenVsString ("Brainfuck output: " ++ baseName) goldenFile output
